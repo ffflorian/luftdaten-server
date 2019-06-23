9b769c14c577ff44fd609dce0def116db6a947ce
Florian Keller <github@floriankeller.de>
2019-06-23 16:59:36 +0200

    feat: Raw display of data

--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,11 +1,18 @@
 import * as express from 'express';
+import * as Knex from 'knex';
 
-import {ServerConfig} from '../config';
+import {inspect} from 'util';
+import {KnexResult, KnexUpdate, TABLE} from '../knex/KnexService';
 
 const router = express.Router();
 
-export const mainRoute = (config: ServerConfig) => {
+export const dataRoute = (knexInstance: Knex<KnexUpdate, KnexResult>) => {
   return router.get('/', async (req, res) => {
-    res.send('hello');
+    const entries: KnexResult[] = await knexInstance
+      .from(TABLE.LUFTDATEN)
+      .select('*')
+      .limit(20);
+    const str = entries.map(entry => inspect(entry, {breakLength: Infinity, depth: Infinity, sorted: true}));
+    res.send(str);
   });
 };
