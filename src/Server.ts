c219bfd294f0c2da8d2df062f7c03d88bbb8ed39
Florian Keller <github@floriankeller.de>
2019-06-23 17:15:49 +0200

    add handlebars

--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
 import * as express from 'express';
+import * as expressHandlebars from 'express-handlebars';
 import * as helmet from 'helmet';
 import * as http from 'http';
 
@@ -20,6 +21,9 @@ export class Server {
 
   async init(): Promise<void> {
     const knexInstance = await this.knexService.init();
+
+    this.app.engine('handlebars', expressHandlebars());
+    this.app.set('view engine', 'handlebars');
 
     this.app.use((req, res, next) => {
       bodyParser.json({limit: '200mb'})(req, res, error => {
--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,18 +1,18 @@
 import * as express from 'express';
 import * as Knex from 'knex';
 
-import {inspect} from 'util';
 import {KnexResult, KnexUpdate, TABLE} from '../knex/KnexService';
 
 const router = express.Router();
 
 export const mainRoute = (knexInstance: Knex<KnexUpdate, KnexResult>) => {
   return router.get('/', async (req, res) => {
-    const entries: KnexResult[] = await knexInstance
+    const result: KnexResult[] = await knexInstance
       .from(TABLE.LUFTDATEN)
       .select('*')
       .limit(20);
-    const str = entries.map(entry => inspect(entry, {breakLength: Infinity, depth: Infinity, sorted: true}));
-    res.send(str);
+    const entries: Array<{values: string[]}> = result.map(entry => ({values: Object.values(entry)}));
+    const headers = Object.keys(result[0]);
+    res.render('main', {entries, headers});
   });
 };
